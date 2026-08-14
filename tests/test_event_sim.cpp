c4dba60a5bd725e32b4090f83b4d0cad579057c8
waqar-ukaea <waqar.butt@ukaea.uk>
2026-08-14 12:20:18 +0100

    Added a regression test on the pwr_pincell

--- tests/test_event_sim.cpp
+++ tests/test_event_sim.cpp
@@ -3,8 +3,32 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#include "xdg/xdg.h"
+#include "particle_sim_event.h"
 
-TEST_CASE("First test")
+
+using namespace xdg;
+
+
+
+TEST_CASE("pwr-pincell event sim test")
 {
+  // Setup XDG
+  std::shared_ptr<XDG> xdg = XDG::create(MeshLibrary::MOAB, RTLibrary::CUBQL);
+  const auto& mm = xdg->mesh_manager();
+  mm->load_file(model_filename);
+  mm->init();
+  mm->parse_metadata();
+  xdg->prepare_raytracer();
+
+  // Setup EventSimulationData
+  EventSimulationData sim_data;
+  sim_data.xdg_ = xdg;
+  sim_data.n_particles_ = 100000;
+  sim_data.max_events_ = 100;
+  sim_data.mfp_ = 0.5;
+
+  transport_particle_event_based(sim_data);
+
 
 }
